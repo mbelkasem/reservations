2d1c6ad376c4b38bc9e69ea4d453a12c57f2e363
MBELKASEM <mohamed.belkasem@gmail.com>
2023-03-24 11:07:49 +0100

    Entité Artists + migration + seeders

---
+++ src/DataFixtures/ArtistFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Artists;
+
+class ArtistFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $artists = [
+            ['firstname'=>'Daniel','lastname'=>'Marcelin'],
+            ['firstname'=>'Philippe','lastname'=>'Laurent'],
+            ['firstname'=>'Marius','lastname'=>'Von Mayenburg'],
+            ['firstname'=>'Olivier','lastname'=>'Boudon'],
+            ['firstname'=>'Anne Marie','lastname'=>'Loop'],
+            ['firstname'=>'Pietro','lastname'=>'Varasso'],
+            ['firstname'=>'Laurent','lastname'=>'Caron'],
+            ['firstname'=>'Élena','lastname'=>'Perez'],
+            ['firstname'=>'Guillaume','lastname'=>'Alexandre'],
+            ['firstname'=>'Claude','lastname'=>'Semal'],
+            ['firstname'=>'Laurence','lastname'=>'Warin'],
+            ['firstname'=>'Pierre','lastname'=>'Wayburn'],
+            ['firstname'=>'Gwendoline','lastname'=>'Gauthier'],
+        ];
+        
+        foreach ($artists as $record) {
+            $artist = new Artists();
+            $artist->setFirstname($record['firstname']);
+            $artist->setLastname($record['lastname']);
+
+            $manager->persist($artist);
+        }
+
+
+        $manager->flush();
+    }
+}
